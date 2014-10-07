f02f50d483e3e97fccf5999bec5b777a0c75b1bd
jjiang18 <vincentjiang.sh@gmail.com>
2014-10-07 16:52:50 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="headerBackground">
+    <div class="headerText">
+        <p> Wishing Well</p>
+    </div>
+</div>
+<div id="loginArea">
+    <div id="loginImage">
+        <img src="wish.jpg" alt="Wish" style="width:1024px;height:700px">
+    </div>
+    <div class="login">
+        <form id="loginSec">
+          Login E-mail:
+          <input type="email" name="logEmail"></br>
+          Password :
+          <input type="password" name="logPassword">
+          </br>
+          <input type="submit" value="Log in">
+        </form>
+    </div>
+    <div class="register">
+        <form id="registerSec">
+          E-mail :
+          <input type="email" name="regEmail"></br>
+          Password :
+          <input type="text" name="regPassword">
+          </br>
+          <input type="submit" value="Register now!">
+        </form>
+    </div>
+</div>
+
+<div class="footBackground">
+    <div  class="footInformation" alt="contact">
+        <h2>Contact people</h2>
+        <p>(arranged alphabetically)</br>
+           Abhishek Harish harish2@illinois.edu</br>
+           Jingwen Jiang jjiang18@illinois.edu</br>
+           Mengying Wang mwang93@illinois.edu</br>
+           Sheena Panthaplackel panthap2@illinois.edu
+        </p>
+    </div>
+
+    <div  class="footInformation" alt="aboutus">
+        <h2>About us</h2>
+        <p>Wishing Well focuses on centralizing wish lists for both formal and informal occasions. This allows people to avoid wasting resources by purchasing items that are not useful or that have already been purchased. In addition, immaterial gestures such as having dinner or attending a concert with someone are also included. Furthermore, wish lists can be shared via social network.</p>
+<!--    <div style="width: 50%"> check 2</div>-->
+</div>
+
+
+</body>
+</html>
